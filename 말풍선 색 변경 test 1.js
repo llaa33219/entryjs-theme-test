96b0a9c80a28fa6c579eacfe635b5462917f6c61
BLOUplanet <147328072+llaa33219@users.noreply.github.com>
2025-09-26 23:08:23 +0900

    Update 말풍선 색 변경 test 1.js

--- 말풍선 색 변경 test 1.js
+++ 말풍선 색 변경 test 1.js
@@ -1,2 +1,6 @@
+if (!EntryStatic.colorSet.canvas) {
+    EntryStatic.colorSet.canvas = {};
+}
+
 EntryStatic.colorSet.canvas.dialog = '#ff6b6b';      // 테두리 색상 (빨간색)
 EntryStatic.colorSet.canvas.dialogBG = '#ffe066';    // 배경 색상 (노란색)
